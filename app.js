42bfe72dbb24e49a861557095a14638b525d3418
Yaron <justame@gmail.com>
2014-09-20 17:00:15 +0300

    working counter

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var casper = require('casper').create({
 	verbose: true,
 	logLevel: 'info',
 	pageSettings: {
-		loadImages: true, // The WebPage instance used by Casper will
-		loadPlugins: true, // use these settings
+		loadImages: false, // The WebPage instance used by Casper will
+		loadPlugins: false, // use these settings
 		userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4',
 		engine: 'slimerjs'
 	},
 	viewportSize: {
 		width: 1504,
-		height: 400
+		height: 600
 	},
 	verbose: true		
 
@@ -55,43 +55,28 @@ then(function() {
 	console.log('click on browse users');
 	this.click('#nav_matches > a');
 }).then(function(){
-	this.viewport(1524, 5068);
+	// this.viewport(1524, 5068);
 }).
 then(function(){
 	this.scrollToBottom()
-}).
-then(function(){
-	this.wait(timeouts.defaultTime, function() {
-			this.scrollToBottom()
-			casper.thenEvaluate(function() {
-				jQuery('link').remove();
-				document.querySelectorAll('body')[0].style.height = '2000px';
-			});
-			this.wait(timeouts.defaultTime, function() {
-				casper.thenEvaluate(function() {
-					document.querySelectorAll('body')[0].style.height = '2000px';
-					console.log('scrolling down');
-					console.log('window.document.body.scrollTop=' + window.document.body.scrollTop);
-					console.log('document.body.scrollHeight=' + document.body.scrollHeight);
-					window.scrollTo(0,99999);
-					window.document.body.scrollTop = document.body.scrollHeight;
-				});
-			})
-
-	});
-}).
-then(function() {
+})
+.then(function() {
 	this.wait(timeouts.defaultTime, function() {
 		casper.thenEvaluate(function() {
 
 			console.log("Page Title " + document.title);
-
+			var users = {};
 			var scrollInterval = setInterval(function() {
 				console.log('scrolling down');
 				console.log('window.document.body.scrollTop=' + window.document.body.scrollTop);
 				console.log('document.body.scrollHeight=' + document.body.scrollHeight);
 				window.scrollTo(0,99999);
 				window.document.body.scrollTop = document.body.scrollHeight;
+				var unlikedUsersButtons = jQuery('button.binary_rating_button.like:not(.liked)');
+				unlikedUsersButtons.each(function(index, elm){
+					var id = jQuery(elm).attr('data-tuid');
+					users[id] = true;
+				})
 			}, 2000);
 
 
@@ -99,19 +84,10 @@ then(function() {
 				clearInterval(scrollInterval);
 				console.log('getting unliked users count');
 				var unlikedUsersButtons = document.querySelectorAll('button.binary_rating_button.like:not(.liked)');
-				console.log(unlikedUsersButtons.length + ' users found');
-			}, (2000 * 30));
+				console.log(_.keys(users).length + ' users found');
+			}, (2000 * 4));
 		});
 	})
-}).then(function() {
-	this.wait(2000 * 32 + 2000 + 4000, function() {
-		this.capture('screenshots/okcupid.png', {
-			top: 0,
-			left: 0,
-			width: 1200,
-			height: 5000
-		});
-	});
 });
 
 
